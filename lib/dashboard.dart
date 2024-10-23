a8d9ac17c8eb20766cc80e952d009e6fdc1af1a9
putramac <craftgirlsssshopping@gmail.com>
2024-10-23 21:17:43 +0700

    last commit v2

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,13 +5,39 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
         automaticallyImplyLeading: false,
       ),
-      body: const Center(
-        child: Text("Selamat datang di Dashboard", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: size.width,
+              height: size.width / 3,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                image: const DecorationImage(
+                  opacity: 0.9,
+                  colorFilter: ColorFilter.mode(Colors.black38, BlendMode.darken),
+                  image: NetworkImage("https://masputra.nextjiesdev.site/assets/mhs/pesawat.jpg"),
+                  fit: BoxFit.cover
+                  )
+                ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("JNE Express Airlines", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.white)),
+                  Text("Antar barang ke berbagai negara dengan mudah dan hemat", textAlign: TextAlign.start, style: TextStyle(fontSize: 14, color: Colors.white70)),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Seminar SMK Senopati',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(backgroundColor: Colors.green, textStyle: const TextStyle(color: Colors.white)),
